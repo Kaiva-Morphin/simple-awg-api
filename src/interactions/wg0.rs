79a0d904597a109ca94628598474ba4660d777fd
kaiv <kaiv@kaiv.space>
2025-08-25 23:49:30 +0300

    fixes

--- src/interactions/wg0.rs
+++ src/interactions/wg0.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
-use tracing::{error, warn};
+use tracing::{error, info, warn};
 
 use crate::interactions::shared::command_in_docker;
 
@@ -81,8 +81,10 @@ impl AwgInterfaceConf {
 
     pub async fn from_docker() -> anyhow::Result<Option<Self>> {
         let output = command_in_docker(&["cat", "/opt/amnezia/awg/wg0.conf"]).await?;
+        info!("Got wg0.conf");
         let data = String::from_utf8_lossy(&output.stdout);
         let pk = command_in_docker(&["cat", "/opt/amnezia/awg/wireguard_server_public_key.key"]).await?;
+        info!("Got public key");
         let public_key = String::from_utf8_lossy(&pk.stdout).into_owned();
         
 
@@ -124,8 +126,10 @@ impl AwgInterfaceConf {
             }
         }
         store_section(&current_section, &current_lines);
-
-        interface_lines.is_empty() && return Ok(None);
+        
+        if interface_lines.is_empty() {
+            return Ok(None)
+        }
 
         let interface = interface_lines.join("\n");
         Ok(Some(Self {
@@ -142,15 +146,15 @@ impl AwgInterfaceConf {
 #[derive(Debug, Deserialize, Clone)]
 pub struct AWGInterfaceData {
     pub port: String,
-    pub jc: u32,
-    pub jmin: u32,
-    pub jmax: u32,
-    pub s1: u32,
-    pub s2: u32,
-    pub h1: u32,
-    pub h2: u32,
-    pub h3: u32,
-    pub h4: u32
+    pub jc: String,
+    pub jmin: String,
+    pub jmax: String,
+    pub s1: String,
+    pub s2: String,
+    pub h1: String,
+    pub h2: String,
+    pub h3: String,
+    pub h4: String
 }
 
 #[derive(Default)]
@@ -168,35 +172,30 @@ impl AWGInterfaceData {
     //     let data = String::from_utf8_lossy(&r.stdout);
     //     Ok(Some(AWGInterfaceData::from_str(&data).ok_or(anyhow::anyhow!("Failed to parse data"))?))
     // } 
-    pub fn raw_from_str(s: &str) -> AWGRaw {
+    fn raw_from_str(s: &str) -> AWGRaw {
         let mut raw = AWGRaw::default();
-        for line in s.lines() {
-            let line = line.trim();
-            if let Some((key, value)) = line.split_once(':') {
-                let key = key.trim();
-                let value = value.trim();
-                match key {
-                    "allowed ips" => {
-                        raw.last_allowed_ip = Some(value.to_string());
-                    }
-                    "public key" => {
-                        raw.public_key = Some(value.to_string());
-                    }
-                    "listening port" | "ListenPort" => {
-                        raw.port = Some(value.to_string());
-                    }
-                    _ => {
-                        raw.map.insert(key, value);
-                    }
-                }
-            }
-        }
+        
         raw
     }
 
     pub fn from_str(s: &str) -> Option<Self> {
         let raw = Self::raw_from_str(s);
-        let port = raw.port?.parse().ok()?;
+
+        // let mut last_allowed_ip = None;
+        // let mut public_key = None;
+        // let mut port = None;
+        let mut map = HashMap::new();
+
+        for line in s.lines() {
+            let line = line.trim();
+            if let Some((key, value)) = line.split_once(':') {
+                let key = key.trim();
+                let value = value.trim();
+                map.insert(key.to_lowercase(), value);
+            }
+        }
+
+        let port = raw.port?;
         Some(AWGInterfaceData {
             port,
             jc: raw.map.get("jc")?.parse().ok()?,
